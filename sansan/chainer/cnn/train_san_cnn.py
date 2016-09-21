2626ee41b4374ebaeb8b6ced4b111ac572ac231b
You <you@example.com>
2016-09-21 10:38:44 +0900

    訓練データ作成処理をコピペ

--- sansan/chainer/cnn/train_san_cnn.py
+++ sansan/chainer/cnn/train_san_cnn.py
@@ -11,6 +11,147 @@ import chainer.links as L
 from chainer import training
 from chainer.training import extensions
 
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier#ExtraTreesClassifier
+from sklearn.grid_search import GridSearchCV
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import log_loss
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.externals import joblib
+from datetime import datetime
+from evolutionary_search import EvolutionaryAlgorithmSearchCV
+from sklearn.cross_validation import StratifiedKFold
+from xgboost import XGBClassifier
+import math
+from sklearn.metrics import mean_absolute_error
+
+import os
+import time
+
+
+from time import clock
+from PIL import Image
+
+fMakeTrain = 0
+fDoMode = 0
+
+if fMakeTrain == 1:
+    # 予測出力ファイル
+    fn_prediction = 'prediction.csv'
+    
+    # ダウンロードデータ
+    train_csv    = '../../train.csv'
+    train_images = '../../train_images'
+    test_csv     = 'test.csv'
+    test_images  = 'test_images'
+    
+    _columns = ['company_name', 'full_name', 'position_name', 'address', 'phone_number', 'fax', 'mobile', 'email', 'url']
+    
+    _img_len = 96
+    
+    _batch_size = 128
+    _nb_epoch   = 20
+    _sgd_lr     = 0.1
+    _sgd_decay  = 0.001
+    _Wreg_l2    = 0.0001
+    
+    def _load_rawdata(df, dir_images):
+        '''画像を読み込み、4Dテンソル (len(df), 1, _img_len, _img_len) として返す
+        '''
+    
+        X = np.zeros((len(df), 1, _img_len, _img_len), dtype=np.float32)
+    
+        for i, row in df.iterrows():
+            img = Image.open(os.path.join(dir_images, row.filename))
+            img = img.crop((row.left, row.top, row.right, row.bottom))
+            img = img.convert('L')
+            img = img.resize((_img_len, _img_len), resample=Image.BICUBIC)
+    
+            # 白黒反転しつつ最大値1最小値0のfloat32に画素値を正規化
+            img = np.asarray(img, dtype=np.float32)
+            b, a = np.max(img), np.min(img)
+            X[i, 0] = (b-img) / (b-a) if b > a else 0
+    
+        return X
+    
+    def load_train_data():
+        df = pd.read_csv(train_csv)
+        X = _load_rawdata(df, train_images)
+        Y = df[_columns].values
+        return X, Y
+    
+    X, y = load_train_data()
+
+    joblib.dump(X,"X.pkl")
+    joblib.dump(y,"y.pkl")
+else:
+    X = joblib.load("X.pkl")
+    y = joblib.load("y.pkl")
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.8, random_state=1234)
+print('学習データの数:', len(X_train))
+print('検証データの数:', len(X_test))
+
+"""
+fMakeTrain = 0
+fDoMode = 0
+
+if fMakeTrain == 1:
+    def load_data(file_name, img_dir, img_shape, orientations, pixels_per_cell, cells_per_block):
+        classes = ['company_name', 'full_name', 'position_name', 'address', 'phone_number', 'fax', 'mobile', 'email', 'url']
+        df = pd.read_csv(file_name)
+        n = len(df)
+        Y = np.zeros((n, len(classes)))
+        print('loading...')
+        s = clock()
+        for i, row in df.iterrows():
+            f, l, t, r, b = row.filename, row.left, row.top, row.right, row.bottom
+            print(i,f)
+            img = Image.open(os.path.join(img_dir, f)).crop((l,t,r,b)) # 項目領域画像を切り出す
+            if img.size[0]<img.size[1]:                                # 縦長の画像に関しては90度回転して横長の画像に統一する
+                img = img.transpose(Image.ROTATE_90)
+            
+            # preprocess
+            img_gray = img.convert('L')
+            img_gray = np.array(img_gray.resize(img_shape))/255.       # img_shapeに従った大きさにそろえる
+    
+    
+            # feature extraction
+            img = np.array(hog(img_gray,orientations = orientations,
+                               pixels_per_cell = pixels_per_cell,
+                               cells_per_block = cells_per_block))
+            if i == 0:
+                feature_dim = len(img)
+                print('feature dim:', feature_dim)
+                X = np.zeros((n, feature_dim))
+            
+            X[i,:] = np.array([img])
+            y = list(row[classes])
+            Y[i,:] = np.array(y)
+        
+        print('Done. Took', clock()-s, 'seconds.')
+        return X, Y
+        
+    
+    img_shape = (216,72)
+    orientations = 6
+    pixels_per_cell = (12,12)
+    cells_per_block = (1, 1)
+    X, y = load_data('../../train.csv', '../../train_images', img_shape, orientations, pixels_per_cell, cells_per_block)
+#    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.5, random_state=1234)
+    joblib.dump(X,"X.pkl")
+    joblib.dump(y,"y.pkl")
+else:
+    X = joblib.load("X.pkl")
+    y = joblib.load("y.pkl")
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.8, random_state=1234)
+print('学習データの数:', len(X_train))
+print('検証データの数:', len(X_test))
+"""
 
 # Network definition
 class MLP(chainer.Chain):
@@ -24,16 +165,18 @@ class MLP(chainer.Chain):
         self.train = True
     
 #    def forward(x_data, y_data, train=True):
-    def __call__(self, x, t):
-        x, t = chainer.Variable(x), chainer.Variable(t)
+    def __call__(self, x):#, t):
+#        x, t = chainer.Variable(x), chainer.Variable(t)
+        x = chainer.Variable(x)
         h = F.max_pooling_2d(F.relu(self.conv1(x)), 2)
         h = F.max_pooling_2d(F.relu(self.conv2(h)), 2)
         h = F.dropout(F.relu(self.l1(h)), train=self.train)
         y = self.l2(h)
-        if self.train:
-            return F.softmax_cross_entropy(y, t)
-        else:
-            return F.accuracy(y, t)
+        return y
+#        if self.train:
+#            return F.softmax_cross_entropy(y, t)
+#        else:
+#            return F.accuracy(y, t)
 
 """
     def __init__(self, n_in, n_units, n_out):
